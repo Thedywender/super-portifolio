221ae60368d2dfa98c8f5dea2ec4e9692090e891
Thedywender <thedywender@hotmail.com>
2024-06-06 19:47:20 -0400

    create template create_profile add forms

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 User = get_user_model()
 
@@ -9,3 +10,9 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["name", "github", "linkedin", "bio"]
